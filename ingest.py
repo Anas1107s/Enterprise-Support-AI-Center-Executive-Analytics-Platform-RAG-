05310e2472240b06bd91b87cc552128d14752fd7
Anas <anasss1107@gmail.com>
2026-06-19 20:09:36 +0530

    update

--- ingest.py
+++ ingest.py
@@ -1,18 +1,20 @@
 import os
-from langchain_community.document_loaders import TextLoader
+from langchain_community.document_loaders import DirectoryLoader , TextLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_community.embeddings import OllamaEmbeddings
+#from langchain_community.embeddings import OllamaEmbeddings
 from langchain_community.vectorstores import Chroma
+from langchain_community.embeddings import HuggingFaceEmbeddings
+
 
 print("Loading company documents...")
-loader = TextLoader("knowledge_base/policies.txt")
+loader = DirectoryLoader("knowledge_base", glob="*.txt",loader_cls=TextLoader)
 documents = loader.load()
 
 text_splitter = RecursiveCharacterTextSplitter(chunk_size=500,chunk_overlap=50)
 chunks = text_splitter.split_documents(documents)
 print(f"split document into {len(chunks)} individual text chunks.")
 
-embeddings = OllamaEmbeddings(model="nomic-embed-text")
+embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
 
 print("Converting text chunks to vectors and saving to ChromaDB...")
 persist_directory = "./chroma_db"
